df4388ac6193f03df1b2d1bd6df6242b2d3f91a5
kenneth <kennethkosgei38@gmail.com>
2025-06-19 10:12:48 +0300

    objectslab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+let employee = { name: "Sam", streetAddress: "11 Broadway" };
+
+// 1. Non-destructive key/value update
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return { ...obj, [key]: value };
+}
+
+// 2. Destructive key/value update
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+// 3. Non-destructive delete
+function deleteFromEmployeeByKey(obj, key) {
+  let newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+}
+
+// 4. Destructive delete
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
 // Write your solution in this file!
